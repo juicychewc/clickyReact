31ee0707ca33a1b9baa6b16d51ef957895f78a28
Sushichan <collin.chew.7188@gmail.com>
2018-10-20 19:31:19 -0700

    Updates

---
+++ precache-manifest.4c2d2579dda3dfc4db7d8f08879b62ab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9117fcced3c3ee934457",
+    "url": "/clickyReact/static/js/runtime~main.9117fcce.js"
+  },
+  {
+    "revision": "09e78cb82a9e222b015b",
+    "url": "/clickyReact/static/js/main.09e78cb8.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/clickyReact/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "09e78cb82a9e222b015b",
+    "url": "/clickyReact/static/css/main.ac3da02a.chunk.css"
+  },
+  {
+    "revision": "7ac7fd9e68fbf0fea1693365d85b93ad",
+    "url": "/clickyReact/index.html"
+  }
+];
